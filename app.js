f7da6a21323b6243628cc1f7feba192fd0534670
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-11-19 14:29:16 -0500

    1/ Function added to refactor returned object 2/ Refactored object is used to load different data for each of the sections for each planet 3/ Logic to toggle active menu items

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
-renderHtml('mercury');
+let currentPlanet = 'mercury';
+let currentSection = 'overview';
+renderHtml(currentPlanet);
 
+// construct variables for main DOM elements
 const mainElement = document.getElementById('js__main');
+const navbarPrimary = document.getElementById('js__navbar-primary');
+const navbarSecondaryMobile = document.getElementById('js__navbar-secondary--mobile');
 const footer = document.getElementById('js__footer');
 
+// add event listener logic to primary navigation links
 document.querySelectorAll('.navbar--mobile__planets ~ a')
 .forEach((e) => {
   e.addEventListener('click', () => {
+    
     renderHtml(e.innerHTML);
-    document.getElementById('navbarNav').classList.toggle('show');
-  })
+    // close the navbar
+    navbarPrimary.querySelector('.collapse').classList.toggle('show');
+    // remove active class from previous planet
+    navbarPrimary.querySelector(`.${currentPlanet} ~ a`).classList.toggle('active');
+    // add active class to new planet
+    e.classList.toggle('active');
+    // set current planet to new planet
+    currentPlanet = e.innerHTML.toLowerCase();
+
+    // resets secondary menun to 'overview' section
+    let id = `#${currentSection}`;
+    navbarSecondaryMobile.querySelector(id).classList.toggle('active');
+    navbarSecondaryMobile.querySelector('#overview').classList.toggle('active');
+    currentSection = 'overview';
+  });
 });
 
+// add event listener to secondary navigation links
+navbarSecondaryMobile.querySelectorAll('a')
+.forEach((e) => {
+    e.addEventListener('click', () => {
+      let sectionName = e.innerHTML.toLowerCase();
+      if (sectionName === 'surface') {
+        renderHtml(currentPlanet, 'geology');
+      } else {
+        renderHtml(currentPlanet, sectionName);
+      }
+      let id = `#${currentSection}`;
+
+      navbarSecondaryMobile.querySelector(id).classList.toggle('active');
+      
+      e.classList.toggle('active');
+
+      currentSection = sectionName;
+    })
+})
+
+// read data from json file and return as objects
 async function readJsonFile() {
   const data = await fetch('./data.json')
-  return response = data.json();
+  return data.json();
 }
 
-// search through the data for the planet
+// search through array of objects using
+// return single object where key name matches planet name
 async function searchJson(key, value) {
   const json = await readJsonFile();
   for (let i = 0; i < json.length; i++) {
@@ -26,14 +68,44 @@ async function searchJson(key, value) {
   };
 };
 
-async function renderHtml(value) {
-  const data = await searchJson('name', value);
-  mainElement.innerHTML = Mustache.render(await renderTemplate('overview'), data);
-  footer.innerHTML = Mustache.render(await renderTemplate('footer'), data);
-};
+// refactor planetObject to match placeholders in mustache template
+// returns newObject where data matches section called by click
+function processObject(section, object) {
+  let image;
+  switch (section) {
+    case 'overview':
+      image = 'planet'
+      break;
+    case 'structure':
+      image = 'internal'
+      break;
+    case 'geology':
+      image = 'geology'
+      break;
+    default:
+      break;
+  }
+  const newObject = {};
+  newObject['name'] = object.name;
+  newObject['content'] = object[`${section}`]['content'];
+  newObject['source'] = object[`${section}`]['source'];
+  newObject['imageSource'] = object['images'][`${image}`];
+  newObject['imageHeight'] = object.height;
+  return newObject;
+}
 
+// fetch mustache template from file using fileName
+// convert to text and return
 async function renderTemplate(templateName) {
   const response = await fetch(`./${templateName}.mustache`);
   const template = await response.text();
   return template;
 }
+
+// use template and object to modify innerHTML
+async function renderHtml(planetName, sectionName = 'overview') {
+  const data = await searchJson('name', planetName);
+  const refactoredObject = await processObject(sectionName, data);
+  mainElement.innerHTML = Mustache.render(await renderTemplate('main'), refactoredObject);
+  footer.innerHTML = Mustache.render(await renderTemplate('footer'), data);
+};
